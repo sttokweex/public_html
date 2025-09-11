54f89b31af73da12c0ef3c75b3f5f17821f29984
Кирилл <kirill-kent2006@mail.ru>
2025-09-11 18:59:48 +0700

    change

--- slot/gameSlot.php
+++ slot/gameSlot.php
@@ -150,7 +150,7 @@ if (!is_array($games)) {
             balance: userBalance
         });
 
-        const authUrl = 'http://localhost:8940/userAuth';
+        const authUrl = 'http://51.250.83.228:2200/userAuth';
 
         try {
             var response = await fetch(authUrl, {
