a68a5961b412638cca086d804311a43025f35ad8
Кирилл <kirill-kent2006@mail.ru>
2025-10-02 18:08:24 +0700

    deploy rdy

--- slot/gameSlot.php
+++ slot/gameSlot.php
@@ -103,7 +103,7 @@ $gamename = (string)$gameid; // Fallback: использовать gameid как
                     lobbyUrl: window.location.origin + '/slot',
                     balance: userBalance
                 });
-                authUrl = 'http://localhost:8940/userAuth';
+                authUrl = 'http://5.129.253.12:2002/userAuth';
             } else {
                 postData = new URLSearchParams({
                     agentID: 'frenzycazUSD',
@@ -113,7 +113,7 @@ $gamename = (string)$gameid; // Fallback: использовать gameid как
                     gameid: game?.gameid ?? gameName,
                     lobbyUrl: "https://frenzycaz.online/slot"
                 });
-                authUrl = 'http://localhost:8940/slot/api/userAuthPP.php';
+                authUrl = 'http://5.129.253.12:2002/slot/api/userAuthPP.php';
             }
 
             try {
--- system/connect.php
+++ system/connect.php
@@ -1,9 +1,24 @@
 <?php
 
-$server = "localhost";
-$login = 'root';
-$pass = 'MADBREAK1';
-$name = 'asino777';
+// $server = "localhost";
+// $login = 'root';
+// $pass = 'MADBREAK1';
+// $name = 'asino777';
+
+// $connection = mysqli_connect($server, $login, $pass)
+//   or die();
+
+// mysqli_select_db($connection, $name)
+//   or die();
+
+// mysqli_query($connection, "SET NAMES utf8"); 
+
+
+
+$server = getenv('DB_HOST');
+$login = getenv('DB_USER');
+$pass = getenv('DB_PASSWORD');
+$name = getenv('DB_NAME');
 
 $connection = mysqli_connect($server, $login, $pass)
   or die();
@@ -12,18 +27,3 @@ mysqli_select_db($connection, $name)
   or die();
 
 mysqli_query($connection, "SET NAMES utf8");
-// <?php
-
-
-// $server = getenv('DB_HOST');
-// $login = getenv('DB_USER');
-// $pass = getenv('DB_PASSWORD');
-// $name = getenv('DB_NAME');
-
-// $connection = mysqli_connect($server, $login, $pass)
-//   or die();
-
-// mysqli_select_db($connection, $name)
-//   or die();
-
-// mysqli_query($connection, "SET NAMES utf8");
