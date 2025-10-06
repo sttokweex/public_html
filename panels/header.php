adbf22bc343325f7e5d9f22b6308edfe3bb78665
Кирилл <kirill-kent2006@mail.ru>
2025-10-06 16:56:24 +0700

    img heihgt

--- panels/header.php
+++ panels/header.php
@@ -431,7 +431,7 @@ $actual_link = "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
                   <a id="bonusBox" href="/bonus" class="dropdown-item"><svg data-ds-icon="Gift" width="20" height="20" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none" class="inline-block shrink-0"><!--[!--><!--]-->
                       <path fill="currentColor" d="M21 5h-3.35c.22-.46.35-.96.35-1.5C18 1.57 16.43 0 14.5 0c-.98 0-1.86.41-2.5 1.06A3.5 3.5 0 0 0 9.5 0C7.57 0 6 1.57 6 3.5c0 .54.13 1.04.35 1.5H3c-1.1 0-2 .9-2 2v1c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2m-6.5-3c.83 0 1.5.67 1.5 1.5S15.33 5 14.5 5 13 4.33 13 3.5 13.67 2 14.5 2M8 3.5C8 2.67 8.67 2 9.5 2s1.5.67 1.5 1.5S10.33 5 9.5 5 8 4.33 8 3.5M3 21c0 1.1.9 2 2 2h6V12H3zm10 2h6c1.1 0 2-.9 2-2v-9h-8z"></path>
                     </svg><span><?php echo $translations['bonus'] ?></span></a>
-                  <a id="referrals" href="/referrals" class="dropdown-item">
+                  <a id="referrals" href="/referals" class="dropdown-item">
                     <svg data-ds-icon="Affiliate" width="20" height="20" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none" class="inline-block shrink-0"><!---->
                       <path fill="currentColor" d="M12 11a5 5 0 1 0 0-10 5 5 0 0 0 0 10"></path>
                       <path fill="currentColor" fill-rule="evenodd" d="M16.5 21v-.14l-.91-.65c-.5.19-1.04.29-1.59.29-2.48 0-4.5-2.02-4.5-4.5 0-1.16.45-2.2 1.17-3H9c-4.42 0-8 3.58-8 8 0 1.1.9 2 2 2h13.99c-.3-.61-.49-1.28-.49-2" clip-rule="evenodd"></path>
