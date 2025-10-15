f790e5ba2902625e03bbcbb8f546e622ffeffafd
Кирилл <kirill-kent2006@mail.ru>
2025-10-15 17:13:49 +0700

    slot

--- slot/gameSlot.php
+++ slot/gameSlot.php
@@ -435,7 +435,7 @@ $gamename = (string)$gameid; // Fallback: использовать gameid как
                     agentID: 'frenzycazUSD',
                     userID: userId,
                     isaffiliate: 'true',
-                    lang: 'en',
+                    lang: 'us',
                     gameid: game?.gameid,
                     lobbyUrl: "https://frenzycaz.online/slot"
                 });
